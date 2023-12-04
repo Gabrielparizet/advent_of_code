c57b6d12f2d8fb0a69e2af00b00677bc0659aba8
Gabrielparizet <parizetgabriel@gmail.com>
2023-12-04 12:30:54 +0100

    day2 in progress

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -7,57 +7,90 @@ defmodule Day2 do
 
   def possible? do
     input = File.read!("/Users/gabrielparizet/workspace/advent_of_code/day2/lib/input2.txt")
-    |> make_map()
-    # |> do_possible?()
+    input_list = String.split(input, "\n")
+    make_map(input_list)
   end
-
-
 
   ### MAKE_MAP()
 
-  defp make_map(input) do
-    input
-    |> String.split("\n")
-    |> Enum.map(&(make_game(&1)))
-
+  defp make_map(input_list) do
+    %{games: Enum.reduce(input_list, %{}, fn x, acc ->
+      Map.put(acc, get_id(x), get_sets(x))
+    end)}
   end
 
-  defp make_game(map) do
-    map
-    |> String.split([":", ";"])
-    |> get_game()
-  end
-
-  defp get_game([id | sets]) do
-    %{id: get_id(id), sets: Enum.map(sets, &(make_sets(&1)))}
-  end
-
-  defp get_id(id) do
-    id
-    |> String.split(["Game", " "], trim: true)
+  def get_id(line) do
+    line
+    |> String.split([":"])
+    |> List.first()
+    |> String.split("Game ", trim: true)
     |> List.to_string()
     |> String.to_integer()
   end
 
-  defp make_sets(sets) do
-    String.split(sets, ",")
-    |> Enum.map(&make_pick(&1))
+  defp get_sets(line) do
+    line
+    |> String.split([":"])
+    |> get_tail()
+    |> make_sets_map()
   end
 
-  defp make_pick(pick) do
-    String.split(pick, " ", trim: true)
-    |> get_color_and_number
+  defp get_tail([_hd | tl]) do
+    List.to_string(tl)
   end
 
-  defp get_color_and_number([number | color]) do
-    %{number: String.to_integer(number), color: List.to_string(color)}
+  defp make_sets_map(list) do
+    String.split(list, ";")
   end
 
 
 
-  ### DO_POSSIBLE?()
+  # defp do_make_games_map([hd | tail]) do
+  #   id = String.split(hd, "Game ") |> List.to_string() |> String.to_integer()
+  #   sets = tail
+  #   %{id: id, sets: sets}
+  # end
 
-  defp do_possible?(map) do
-    map.sets
-  end
+  # defp build_id_and_sets([id | sets]) do
+  #   %{}
+  # end
+
+  # defp make_game(map) do
+  #   map
+  #   |> String.split([":", ";"])
+  #   |> get_game()
+  # end
+
+  # defp get_game([id | sets]) do
+  #   %{id: get_id(id), sets: Enum.map(sets, &(make_sets(&1)))}
+  # end
+
+  # defp get_id(id) do
+  #   id
+  #   |> String.split(["Game", " "], trim: true)
+  #   |> List.to_string()
+  #   |> String.to_integer()
+  # end
+
+  # defp make_sets(sets) do
+  #   String.split(sets, ",")
+  #   |> Enum.map(&make_pick(&1))
+  # end
+
+  # defp make_pick(pick) do
+  #   String.split(pick, " ", trim: true)
+  #   |> get_color_and_number
+  # end
+
+  # defp get_color_and_number([number | color]) do
+  #   %{number: String.to_integer(number), color: List.to_string(color)}
+  # end
+
+
+
+  # ### DO_POSSIBLE?()
+
+  # defp do_possible?(map) do
+  #   map.sets
+  # end
 end
