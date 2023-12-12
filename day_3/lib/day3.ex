258911ffc3bbdc324eed4b685679ff71549784d0
Gabrielparizet <parizetgabriel@gmail.com>
2023-12-12 12:11:29 +0100

    test(day3): Started writing tests on day3 impl

--- day_3/lib/day3.ex
+++ day_3/lib/day3.ex
@@ -1,212 +1,104 @@
-# defmodule Day3 do
-
-#   @numbers ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-#   # @dir {x, y}
-#   @top_left {-1, -1}
-#   @top {0, -1}
-#   @top_right {1, -1}
-#   @left {-1, 0}
-#   @right {1, 0}
-#   @bottom_left {-1, 1}
-#   @bottom {0, 1}
-#   @bottom_right {1, 1}
+defmodule Day3 do
 
 
-#   @directions [@top_left, @top, @top_right, @left, @right, @bottom_left, @bottom, @bottom_right]
+  @top_left {-1, -1}
+  @top {-1, 0}
+  @top_right {-1, 1}
+  @left {0, -1}
+  @right {0, 1}
+  @bottom_left {1, -1}
+  @bottom {1, 0}
+  @bottom_right {1, 1}
 
-#   def navigate() do
-#     input = File.read!("/Users/gabrielparizet/workspace/advent_of_code/day_3/lib/input2.txt")
-#     |> parse()
-#     numbers_map = get_numbers(input)
-#     directions = @directions
-#     inspect_nbs_map(numbers_map, directions, input)
-#   end
+  @directions [@top_left, @top, @top_right, @left, @right, @bottom_left, @bottom, @bottom_right]
 
+  def test do
+    File.stream!("/Users/gabrielparizet/workspace/advent_of_code/day_3/lib/input2.txt")
+    |> Enum.map(fn line -> String.trim(line) end)
+    |> Enum.map(&(String.codepoints(&1)))
+    |> create_base_grid()
+    |> extract_part_numbers()
+  end
 
-#   def parse(input) do
-#     input
-#     |> String.split("\n")
-#     |> Enum.with_index()
-#     |> Enum.map(fn {val, y} ->
-#       x_values = String.split(val, "", trim: true)
-#       |> Enum.with_index()
-#       |> Enum.map(fn {value, x} ->
-#         %{{x, y} => value}
-#       end)
-#     end)
-#     |> List.flatten()
-#     |> Enum.reduce(%{}, fn map, acc ->
-#       Map.merge(acc, map)
-#     end)
-#   end
+  def create_base_grid(lines) do
+    lines
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, &create_grid_line/2)
+  end
 
-#   def get_numbers(input) do
-#     numbers_map = Enum.reduce(input, %{}, fn {key, val}, acc ->
-#       case String.contains?(val, @numbers) do
-#         true -> Map.put_new(acc, key, val)
-#         _ -> Map.put_new(acc, :not_a_number, "not_a_number")
-#       end
-#     end)
-#     numbers_map = Map.delete(numbers_map, :not_a_number)
-#   end
+  def create_grid_line({chars, y}, grid) do
+    Enum.with_index(chars)
+      |> Enum.reduce(grid, &(create_grid_column(y, &1, &2)))
+  end
 
-#   defp inspect_nbs_map(numbers_map, directions, input) do
-#     Enum.reduce(numbers_map, %{}, fn {{x, y}, char} ->
-#       new_coord = Enum.reduce(@directions fn {x_dir, y_dir} ->
-#         {x_dir + x, y_dir + y}
-#       end)
-#     end)
-#   end
+  def create_grid_column(y, {char, x}, grid) do
+    Map.put(grid, {y, x}, char)
+  end
 
-# end
+  def extract_part_numbers(grid) do
+    Map.keys(grid)
+    |> Enum.reduce({grid, []}, fn yx, {grid, part_numbers} ->
+      case get_char_type(Map.get(grid, yx)) do
+        :symbol -> fetch_part_numbers(yx, grid, part_numbers)
+        _ -> {grid, part_numbers}
+      end
+    end)
+  end
 
+  def get_char_type("."), do: :point
+  def get_char_type(char) do
+    if Regex.match?(~r/\d/, char) do
+      :digit
+    else
+      :symbol
+    end
+  end
 
-  # def parse() do
-  #   input = File.read!("/Users/gabrielparizet/workspace/advent_of_code/day_3/lib/input2.txt")
-  #   |> String.split("\n")
-  #   |> Enum.with_index(1)
-  #   |> Enum.map(&(create_matrix(&1)))
-  #   |> Enum.map(&(group_numbers(&1)))
-  # end
+  def fetch_part_numbers(yx, grid, part_numbers) do
+    Enum.reduce(@directions, {grid, part_numbers}, fn direction, acc ->
+      at = move_cursor(yx, direction)
+      case is_digit?(grid, at) do
+        true ->
+          {new_grid, integer} = build_part_numbers(at, grid)
+          {new_grid, [integer | acc]}
+        _ -> {grid, acc}
+      end
+    end)
+  end
 
-  # defp create_matrix({line, row_id}) do
-  #   column = String.split(line, "", trim: true)
-  #   |> Enum.with_index(1)
-  #   Enum.map(column, fn {char, column_id} ->
-  #     %{char: char, row_id: row_id, column_id: column_id}
-  #   end)
-  # end
+  def move_cursor({y, x}, {y_dir, x_dir}) do
+    {y + y_dir, x + x_dir}
+  end
 
-  # defp group_numbers(line) do
-  #   Enum.map(line, fn map ->
-  #     case String.contains?(map.char, @numbers) do
-  #       true -> map
-  #       false -> "not_a_number"
-  #     end
-  #   end)
-  #   |> Enum.filter(fn x -> x != "not_a_number" end)
-  #   |> Enum.reduce([], fn%{char: char, row_id: row_id, column_id: column_id} = current_map, acc ->
-  #     case acc do
-  #       [] ->
-  #         acc = List.insert_at([Map.update!(current_map, :column_id, fn x -> [x] end) | acc ]
-  #         IO.inspect(acc, label: "CURRENT acc EMPTY CASE")
-  #       [%{char: prev_char, row_id: _row_id, column_id: prev_column_ids} = previous_acc | rest]  ->
-  #         IO.inspect(previous_acc)
-  #         IO.inspect(current_map, label: "CURRENT MAP EMPTY CASE")
-  #         if List.first(prev_column_ids) + 1 == column_id do
-  #           Map.update!(current_map, :char, fn current_char -> Enum.join([prev_char | current_char]) end)
-  #           Map.update!(current_map, :column_id, fn current_column_id -> Enum.reverse([current_column_id | prev_column_ids]) end)
-  #           List.replace_at(acc, 0, current_map)
-  #         else
-  #           [rest | current_map]
-  #         end
-  #     end
-  #     # IO.inspect(acc, label: "acc2")
-  #     # IO.inspect(acc, label: "turn2")
-  #   end)
-  # end
+  def is_digit?(grid, at) do
+    Map.has_key?(grid, at) && Regex.match?(~r/\d/, Map.get(grid, at))
+  end
 
-  # def parse() do
-  #   input = File.read!("/Users/gabrielparizet/workspace/advent_of_code/day_3/lib/input2.txt")
-  #   |> String.split("\n")
-  #   line_list = Enum.map(input, &(String.split(&1, "", trim: true)))
-  #   |> Enum.map(&(Enum.with_index(&1)))
-  #   |> Enum.with_index()
-  #   |> Enum.map(fn {line, index} -> %{index: index, line: line} end)
+  def build_part_numbers(at, grid) do
+    IO.inspect(at, label: "at", limit: 10)
+    IO.inspect(grid, label: "grid", limit: 10)
+    do_build_part_numbers(at, grid, [])
+    |> IO.inspect(label: "result", limit: 10)
+  end
 
-  #   symbols_list = get_symbols(input)
+  def do_build_part_numbers(at, grid, digits) do
+    cond do
+      is_digit?(grid, move_cursor(at, @left)) ->
+        do_build_part_numbers(move_cursor(at, @left), grid, digits)
+      is_digit?(grid, move_cursor(at, @right)) ->
+          new_digits = [Map.get(grid, at) | digits]
+          new_grid = Map.put(grid, at, ".")
+          do_build_part_numbers(move_cursor(at, @right), new_grid, new_digits)
+      true -> new_digits = [Map.get(grid, at) | digits]
+              new_grid = Map.put(grid, at, ".")
+              {new_grid, join_part_numbers(new_digits)}
+    end
+  end
 
-  #   line_numbers_list = Enum.map(input, &(String.split(&1, ["." | symbols_list], trim: true)))
+  def join_part_numbers(digits) do
+    digits
+    |> Enum.reverse()
+    |> Enum.join()
+  end
 
-  #   parsed_data = Enum.map(line_list, fn %{index: index, line: line} ->
-  #     %{
-  #       index: index,
-  #       symbols_index: get_symbols_index(line, symbols_list),
-  #       numbers: %{
-  #         index: get_numbers_index(line)
-  #       }
-  #     }
-  #   end)
-
-  #   Enum.zip(parsed_data, line_numbers_list)
-  #   |> Enum.map(fn {map, numbers_list} ->
-  #     Map.update!(map, :numbers, fn numbers_map ->
-  #       Map.put(numbers_map, :numbers_list, numbers_list)
-  #     end)
-  #   end)
-  #   |> Enum.map(fn line ->
-  #     Map.update!(line, :numbers, fn %{index: index, numbers_list: numbers_list} ->
-  #       group_index_with_number(index, numbers_list)
-  #     end)
-  #   end)
-  #   |> Enum.map(fn line -> extract_isolated_numbers(line) end)
-
-  # end
-
-  # defp get_symbols(input) do
-  #   input
-  #   |> Enum.join()
-  #   |> String.split(["." | @numbers], trim: true)
-  #   |> Enum.uniq()
-  # end
-
-  # defp get_symbols_index(line, symbols_list) do
-  #   line
-  #   |> Enum.map(fn {val, index} ->
-  #     case String.contains?(val, symbols_list) do
-  #       true -> index
-  #       _ -> "not_a_symbol"
-  #     end
-  #   end)
-  #   |> Enum.filter(fn x -> x != "not_a_symbol" end)
-  # end
-
-  # defp get_numbers_index(line) do
-  #   line
-  #   |> Enum.map(fn {val, index} ->
-  #     case String.contains?(val, @numbers) do
-  #       true -> index
-  #       _ -> "not_a_number"
-  #     end
-  #   end)
-  #   |> Enum.filter(fn x -> x != "not_a_number" end)
-  # end
-
-  # defp group_index_with_number(index, numbers_list) do
-  #   length = Enum.map(numbers_list, &(String.length(&1)))
-  #   grouped_indexes = %{index: index, numbers_list: length}
-  #   |> group_indexes()
-  #   Enum.zip(grouped_indexes, numbers_list)
-  #   |> Enum.map(fn {indexes, numbers} ->
-  #     %{indexes: indexes, number: numbers}
-  #   end)
-  # end
-
-  # defp group_indexes(map) do
-  #   group_indexes(map.index, map.numbers_list, [])
-  # end
-
-  # defp group_indexes(_, [], acc) when is_list(acc) do
-  #   Enum.reverse(acc)
-  # end
-
-  # defp group_indexes(index, [count | counts], acc) when is_list(acc) do
-  #   {group, rest} = Enum.split(index, count)
-  #   group_indexes(rest, counts, [group | acc])
-  # end
-
-  # defp extract_isolated_numbers(%{index: _, numbers: numbers_list, symbols_index: symbols_index}) do
-  #   Enum.map(numbers_list, fn map ->
-  #     find_adjacent_numbers(map.indexes, symbols_index)
-  #   end)
-
-  # end
-
-  # defp find_adjacent_numbers(indexes, symbols_indexes) do
-  #   IO.inspect(indexes, label: "indexes")
-  #   IO.inspect(symbols_indexes)
-  #   Enum.filter(indexes, fn idx ->
-  #     Enum.member?([idx - 1, idx, idx + 1], Enum.each(symbols_indexes, fn x -> x end))
-  #   end)
-  # end
+end
